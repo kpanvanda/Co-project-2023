75f705bb90a29e2264c1170224d18d54022354b0
Mayankaniiitd <133681432+Mayankaniiitd@users.noreply.github.com>
2023-05-15 23:35:50 +0530

    Update Assembler code.py

--- Assembler code.py
+++ Assembler code.py
@@ -13,6 +13,7 @@ l = ['add', 'sub', 'mov', 'ld', 'st', 'mul', 'div', 'rs', 'ls', 'xor', 'or', 'an
 reg = {'R0': '000', 'R1': '001', 'R2': '010', 'R3': '011', 'R4': '100', 'R5': '101', 'R6': '110',
           'FLAGS': '111'}
 
+#type functions  .....
 
 def typeA(inst, reg1, reg2, reg3,op):
     if inst in opcode.keys() and  reg1 in reg.keys() and reg2 in reg.keys():
